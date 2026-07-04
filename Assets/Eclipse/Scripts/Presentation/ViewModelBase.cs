142a0828c0c1ea8c9134f6f4bcce42c8fe5ec1b0
Geonheui <pgh5986@gmail.com>
2026-07-04 22:23:30 +0900

    feat: 초기 MVVM ViewModel 베이스 구성

---
+++ Assets/Eclipse/Scripts/Presentation/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Eclipse.Presentation
+{
+    public abstract class ViewModelBase : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+        
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+            IsDisposed = true;
+            OnDispose();
+        }
+
+        protected virtual void OnDispose() 
+        {
+            
+        }
+
+    }
+}
---
+++ Assets/Eclipse/Scripts/Tests/ViewModelBaseTests.cs
@@ -0,0 +1,39 @@
+﻿using Eclipse.Presentation;
+using NUnit.Framework;
+
+namespace Eclipse.Tests
+{
+    public class FakeViewModelBase : ViewModelBase
+    {
+        public int DisposeCount { get; private set; }
+        
+        protected override void OnDispose()
+        {
+            base.OnDispose();
+            DisposeCount++;
+        }
+    }
+    
+    public class ViewModelBaseTests
+    {
+        [Test]
+        public void Dispose_2번호출()
+        {
+            var vm = new  FakeViewModelBase();
+            vm.Dispose();
+            vm.Dispose();
+
+            Assert.AreEqual(1, vm.DisposeCount);
+        }
+
+        [Test]
+        public void Dispose_된결과_확인()
+        {
+            var vm = new  FakeViewModelBase();
+            vm.Dispose();
+            Assert.IsTrue(vm.IsDisposed);
+        }
+        
+        
+    }
+}
